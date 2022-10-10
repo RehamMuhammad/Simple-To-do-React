3a7b47d403fbb644a98988041efd4cbc004795eb
Reham Muhammad <reham.muhammad@g2k-group.com>
2022-10-10 10:24:26 +0200

    Initial Styles

--- src/components/Todo/index.js
+++ src/components/Todo/index.js
@@ -1,8 +1,14 @@
 import React from 'react'
+import "./todo.css"
 
 function Todo() {
   return (
-    <div>Todo</div>
+    <div className='container'>
+        <h5 className='title'>Title</h5>
+        <p className='content'>content</p>
+        <time className='due-date'>10-10-2022</time>
+        <p className='status'>Done</p>
+    </div>
   )
 }
 
--- src/pages/HomePage/index.js
+++ src/pages/HomePage/index.js
@@ -1,10 +1,13 @@
 import React from 'react'
 import { Todo } from '../../components'
+import "./home.css"
 
 function Home() {
   return (
-    <Todo />
-    )
+    <main>
+      <Todo />
+    </main>
+  )
 }
 
 export default Home;
