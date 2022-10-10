c4c256cf79255383c62e747585dbeffd4a94b886
Reham Muhammad <reham.muhammad@g2k-group.com>
2022-10-10 16:27:39 +0200

    Implement Update Task Functionality

--- src/pages/EditPage/index.js
+++ src/pages/EditPage/index.js
@@ -1,19 +1,49 @@
-import React , {useState, useEffect} from 'react'
+import React, { useState, useEffect } from 'react'
+import { useParams } from "react-router-dom";
 
 function Edit() {
-const [tasks, setTasks] = useState(JSON.parse(localStorage.getItem('tasks')));
+    const { id } = useParams();
+    const [tasks, setTasks] = useState(JSON.parse(localStorage.getItem('tasks')));
+    const [task, setTask] = useState(null);
+    const [newTask, setNewTask] = useState(task?.task);
+    const [errors, setErrors] = useState(" ")
+
+    useEffect(() => {
+        let task = tasks.find((task) => task.id === Number(id));
+        setTask(task)
+        setNewTask(task.task)
+    }, [id, tasks]);
 
 
-  return (
-//     <form onSubmit={editTask}>
-//     <input className='form-control' type="text" placeholder='Add New Task' value={newTask} onChange={handleChange} />
-//     <input className='btn-submit' type='submit' value='Edit Task' />
-// </form>
-<h1>rr
-    {    console.log(tasks)
-}
-</h1>
-  )
+    const handleChange = (e) => {
+        setNewTask(e.target.value)
+        console.log(newTask)
+    }
+
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        console.log(errors)
+        setErrors(!newTask ? "You Should Enter a Task Name" : "");
+        console.log(errors)
+        if (!errors) {
+            let mapped = tasks.map(task => {
+                return task.id === Number(id) ? { ...task, task: newTask } : { ...task };
+            });
+            setTasks(mapped);
+        }
+    }
+
+    useEffect(() => {
+        localStorage.setItem('tasks', JSON.stringify(tasks));
+    }, [tasks]);
+    return (
+        <form onSubmit={handleSubmit}>
+            <input className='form-control' type="text" value={newTask} onChange={handleChange} />
+            <input className='btn-submit' type='submit' value='Edit Task' />
+            {console.log({ id, tasks, task })
+            }</form>
+
+    )
 }
 
 export default Edit
